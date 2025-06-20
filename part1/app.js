a25e5f9d2e48066b09576a107bac554d22ea086e
henrysaliba <unknown@student.adelaide.edu.au>
2025-06-20 07:36:35 +0000

    [automatic commit] 2025-06-20 07:36:35 UTC 27.33.95.240

--- part1/app.js
+++ part1/app.js
@@ -22,10 +22,6 @@ async function insertTestData(connection) {
     ('alice123', 'alice@example.com', 'hashed123', 'owner'),
     ('bobwalker', 'bob@example.com', 'hashed456', 'walker'),
     ('carol123', 'carol@example.com', 'hashed789', 'owner')
-
-
-
-
     `);
 
     await connection.execute(`
