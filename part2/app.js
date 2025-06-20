ddf9ac0eee4cf36488135fa56f2fc9d9f46cf5ac
henrysaliba <unknown@student.adelaide.edu.au>
2025-06-20 09:40:37 +0000

    [automatic commit] 2025-06-20 09:40:37 UTC 27.33.95.240

--- part2/app.js
+++ part2/app.js
@@ -8,6 +8,7 @@ const app = express();
 // Middleware
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '/public')));
+app.use(s)
 
 // Routes
 const walkRoutes = require('./routes/walkRoutes');
