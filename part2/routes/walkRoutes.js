06335b9d9dabffb7b6bb1263c5744081910a3398
henrysaliba <unknown@student.adelaide.edu.au>
2025-06-20 10:45:18 +0000

    [automatic commit] 2025-06-20 10:45:18 UTC 27.33.95.240

--- part2/routes/walkRoutes.js
+++ part2/routes/walkRoutes.js
@@ -60,7 +60,9 @@ router.post('/:id/apply', async (req, res) => {
 });
 
 router.get('/mine/dogs', async (req, res) => {
-  if (!req.session.user || req.session.user.role !== 'owner')
+  if (!req.session.user || req.session.user.role !== 'owner') {
+    return res.status(401).json({ error: 'Not authorized'})
+  }
 })
 
 module.exports = router;
