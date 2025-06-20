35ff89a1cd1c42b70ffc1aefbe945d2bb7076f00
henrysaliba <unknown@student.adelaide.edu.au>
2025-06-20 11:51:18 +0000

    [automatic commit] 2025-06-20 11:51:18 UTC 27.33.95.240

--- part2/routes/walkRoutes.js
+++ part2/routes/walkRoutes.js
@@ -77,7 +77,12 @@ router.get('/mine/dogs', async (req, res) => {
 
 router.get('/all/dogs', async (req, res) => {
   try {
-    const [rows] = await db.query
+    const [rows] = await db.query(`
+      SELECT Dogs.dog_id, Dogs.name AS dog_name, Dogs.size, Users.username AS owner_username
+      FROM Dogs
+      JOIN Users ON Dogs.owner_id = Users.user_id
+      `);
+      
   }
 
 module.exports = router;
