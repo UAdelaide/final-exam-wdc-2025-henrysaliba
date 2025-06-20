5fe43376c04c534571b8a1bdbb1e411eab37c9eb
henrysaliba <unknown@student.adelaide.edu.au>
2025-06-20 10:13:26 +0000

    [automatic commit] 2025-06-20 10:13:26 UTC 27.33.95.240

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -62,7 +62,7 @@ router.post('/login', async (req, res) => {
 router.post('/logout', (req, res) => {
   req.session.destroy(err => {
     if (err) {
-      
+      return res.status(500).json({ error: 'Logout failed'})
     }
   })
 });
